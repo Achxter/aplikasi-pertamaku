82a6db35548c4c08de1865606ff02dd13490edbe
Achxter <hansphilemon@gmail.com>
2024-10-16 16:39:31 +0700

    removing localhost from base url

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@ import CommentSection from './components/CommentSection.vue';
 const userId = ref('');
 const users = ref(null);
 const newEmail = ref('');
-const API_BASE_URL = process.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const API_BASE_URL = process.env.VITE_API_BASE_URL;
 console.log('API_BASE_URL:', API_BASE_URL);
 
 const getUser = async () => {
