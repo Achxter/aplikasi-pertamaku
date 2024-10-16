d7a7c6a81534a39f4354140e3cb7a7cb9dffbaaa
Achxter <hansphilemon@gmail.com>
2024-10-16 17:36:35 +0700

    Revert "trying env variable"
    This reverts commit 1ffde00998841718c0403947f1ec0410ebc3154f.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  runtimeConfig: {
-    // Make environment variables accessible here
-    public: {
-      API_BASE_URL: process.env.VITE_API_BASE_URL,
-      // Add more environment variables as needed
-    },
-  },
 })
