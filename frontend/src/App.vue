e3c1cd45e47b521913ba94cc5b176d099756d54a
Achxter <hansphilemon@gmail.com>
2024-10-16 16:16:19 +0700

    trying console.log the env baseurl

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,7 @@ const userId = ref('');
 const users = ref(null);
 const newEmail = ref('');
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log('API_BASE_URL:', API_BASE_URL);
 
 const getUser = async () => {
   try {
